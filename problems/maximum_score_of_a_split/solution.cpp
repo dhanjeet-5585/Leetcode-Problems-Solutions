c7c0126a81f62f40aa8d7263976906b7994e1d78
Dhanjeet Mishra <dhanjeetmishra8252@gmail.com>
2025-12-28 03:15:45 +0000

    Sync LeetCode submission - Maximum Score of a Split (cpp)

---
+++ problems/maximum_score_of_a_split/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums) {
+        int n = nums.size();
+        
+        
+        vector<long long> suffixMin(n);
+        suffixMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min((long long)nums[i], suffixMin[i + 1]);
+        }
+        
+        
+        long long prefixSum = 0;
+        long long ans = LLONG_MIN;
+        
+        for (int i = 0; i < n - 1; i++) {
+            prefixSum += nums[i];
+            long long score = prefixSum - suffixMin[i + 1];
+            ans = max(ans, score);
+        }
+        
+        return ans;
+        
+    }
+};
