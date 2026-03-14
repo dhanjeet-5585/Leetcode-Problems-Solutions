cbcb838d84e9af2abb76bbf9731034f133785b78
Dhanjeet Mishra <dhanjeetmishra8252@gmail.com>
2026-03-14 19:05:59 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- problems/majority_element/solution.cpp
+++ problems/majority_element/solution.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //sort
-        sort(nums.begin(),nums.end());
-        int freq=1;
-        int n=nums.size();
-        int  majority_el=nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-                freq++;
-            }
-            else{
-                freq=1; majority_el=nums[i];
-            }
-                
-            
-            if(freq>n/2){
-                return majority_el;
-            }
+        int count=0;
+        int ele= nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(count==0) ele= nums[i];
+            if(nums[i]== ele) count++;
+            else if(nums[i]!= ele) count--;
             
         }
-        return majority_el;
-            
-        
+        int total=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]== ele) total++;
+        }
+        if(total > nums.size()/2) return ele;
+
+        return -1;
+
         
     }
 };
