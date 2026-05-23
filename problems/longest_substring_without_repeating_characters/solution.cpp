a071cd49ff64c2c4805a94df03e799545b1824f8
Dhanjeet Mishra <dhanjeetmishra8252@gmail.com>
2026-05-23 16:35:36 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (cpp)

--- problems/longest_substring_without_repeating_characters/solution.cpp
+++ problems/longest_substring_without_repeating_characters/solution.cpp
@@ -1,52 +1,36 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int size_str= s.size();
-        int curr_val=0;
-        int maximum= INT_MIN;
-        int sum=0;
-        int latest_index=-1;
-        for (int i=1;i<size_str;i++){
-            char curr_character= s[i];
-            int character_index= i;
-            sum=0;
-            if(latest_index==-1){
-                for(int j=0;j< character_index;j++){
-                    
-                    if(curr_character== s[j]){
-                        latest_index=j;
-                        break;
-                    }else{
-                        sum++;
-                    }
-                }
-                // sum= character_index-1;
-                maximum= max(sum,maximum);
-               
-
-            }else{
-                // sum+= (character_index-1)- latest_index;
-                for(int y= latest_index+1;y< character_index; y++){
-                    if( curr_character== s[y]){
-                        latest_index= y;
-                       
-                        break;
-                    }else{
-                        sum++;
-                    }
-                }
-                maximum= max(sum,maximum);
-                
-
-            }
-            
-        }
-        if(maximum<0){
-            return size_str;
-        }
-        else{
-            return maximum+1;
-        }
         
+        unordered_map<char, int> my_dict;
+
+        int l = 0;
+        int r = 0;
+        int maximum = 0;
+
+        while (r < s.length()) {
+
+            if (my_dict.find(s[r]) != my_dict.end() 
+                && l <= my_dict[s[r]]) {
+
+                l = my_dict[s[r]] + 1;
+
+                my_dict[s[r]] = r;
+
+                maximum = max(maximum, (r - l + 1));
+
+                r++;
+            }
+            else {
+
+                my_dict[s[r]] = r;
+
+                maximum = max(maximum, (r - l + 1));
+
+                r++;
+            }
+        }
+
+        return maximum;
     }
 };
